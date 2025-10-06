a3b6edf2417464bceecb0841a510107e05ef7b41
stanko07 <150948918+stanko07@users.noreply.github.com>
2025-10-06 23:07:16 +0300

    Update candidate_have_skills_dao.py

--- lab4_for_bd/app/dao/candidate_have_skills_dao.py
+++ lab4_for_bd/app/dao/candidate_have_skills_dao.py
@@ -1,6 +1,6 @@
 from app.dao.general_dao import GeneralDAO
 from app.domain import CandidateHaveSkills
-
+#сукавіа ва фівьла тфіждвлтамлдф івтаждлфітв 
 
 class CandidateHaveSkillsDAO(GeneralDAO):
     _domain_type = CandidateHaveSkills
